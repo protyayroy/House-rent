7cec45b3f1b3dcd8a1eb25796fd9c238b13c3dba
Protyay Roy <98158272+Protyay-Roy@users.noreply.github.com>
2022-12-23 22:10:11 +0600

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,48 @@
+
+<footer class="footer">
+    <div class="footer-container">
+        <nav class="footer-nav">
+            <div class="footer-description">
+                <h3 class="footer-description-title">Star Hotels</h3>
+                <p>Hospitality and Comfort are our watchwords</p>
+            </div>
+            <div class="footer-contact-us">
+                <h3 class="footer-description-title">Contact Us</h3>
+                <p class="footer-description-detail">
+                    <img src="./assets/img/map-pin.svg" class="footer-description-icon" alt="star hotel location">
+
+                    <span>23, Fola Osibo, Lekki Phase 1</span>
+                </p>
+                <p class="footer-description-detail">
+                    <img src="./assets/img/phone.svg" class="footer-description-icon" alt="star hotels phone number">
+                    <span>
+                        08185956620</span>
+                </p>
+                <p class="footer-description-detail">
+                    <img src="./assets/img/mail.svg" class="footer-description-icon" alt="star hotels email">
+                    <span>support@starhotels.com</span>
+                </p>
+            </div>
+            <div class="footer-follow-us">
+                <h3 class="footer-description-title">Follow Us</h3>
+                <ul class="footer-follow-us-lists">
+                    <li class="follow-us-list">
+                        <a href="">
+                            <img src="./assets/img/facebook.svg" alt="star hotels facebook page">
+                        </a>
+                    </li>
+                    <li class="follow-us-list">
+                        <a href="">
+                            <img src="./assets/img/twitter.svg" alt="star hotels twitter page">
+                        </a>
+                    </li>
+                    <li class="follow-us-list">
+                        <a href="">
+                            <img src="./assets/img/instagram.svg" alt="star hotels instagram page">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</footer>
