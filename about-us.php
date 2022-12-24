e6da73afad76e24cd5c7fff3be11cc1e684f7dc1
Protyay Roy <98158272+Protyay-Roy@users.noreply.github.com>
2022-12-24 22:29:25 +0600

    Add files via upload

---
+++ about-us.php
@@ -0,0 +1,97 @@
+<?php
+session_start();
+
+include("navbar.php");
+
+?>
+
+<section class="main-content-section">
+    <div class="container">
+        <!-- BSTORE-BREADCRUMB START -->
+        <!-- <div class="bstore-breadcrumb">
+            <a href="index.html">HOMe</a>
+            <span><i class="fa fa-caret-right	"></i></span>
+            <span>About us</span>
+        </div> -->
+        <!-- BSTORE-BREADCRUMB END -->
+
+        <h2 class="page-title about-page-title">About us</h2>
+        <hr>
+
+        <div class="row" style="margin-bottom: 20px;">
+            <div class="col-md-4">
+                <!-- SINGLE-ABOUT-INFO START -->
+                <div class="single-about-info">
+                    <div class="our-company">
+                        <h3>Our company</h3>
+                        <strong>Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor
+                            incididun.</strong>
+                        <p>Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor
+                            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Lorem ipsum dolor
+                            sit amet conse ctetur adipisicing elit.</p>
+                        <div class="company-list-menu">
+                            <ul>
+                                <li>Top quality products</li>
+                                <li>Best customer service</li>
+                                <li>30-days money back guarantee</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <!-- SINGLE-ABOUT-INFO END -->
+            </div>
+            <div class="col-md-4">
+                <div class="single-about-info">
+                    <!-- OUR-TEAM START -->
+                    <div class="our-team">
+                        <h3>Our team</h3>
+                        <strong>Lorem set sint occaecat cupidatat non </strong>
+                        <p>Eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                            nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.</p>
+                        <p>Eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                            nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.</p>
+                        <p>Eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                            nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.</p>
+                    </div>
+                    <!-- OUR-TEAM END -->
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="single-about-info">
+                    <!-- OUR-TESTIMONIALS START -->
+                    <div class="our-testimonials">
+                        <h3>Testimonials</h3>
+                        <!-- SINGLE-TESTIMONIALS START -->
+                        <div class="single-testimonials">
+                            <div class="testimonials-text">
+                                <span class="before"></span>
+                                Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim.
+                                <span class="after"></span>
+                            </div>
+                        </div>
+                        <p>Lorem ipsum dolor sit</p>
+                        <!-- SINGLE-TESTIMONIALS END -->
+                        <!-- SINGLE-TESTIMONIALS START -->
+                        <div class="single-testimonials">
+                            <div class="testimonials-text">
+                                <span class="before"></span>
+                                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                                mollit anim id est laborum. Lorem ipsum dolor sit amet conse ctetur adipisicing
+                                elit. Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod.
+                                <span class="after"></span>
+                            </div>
+                        </div>
+                        <p>Ipsum dolor sit</p>
+                        <!-- SINGLE-TESTIMONIALS END -->
+                    </div>
+                    <!-- OUR-TESTIMONIALS END -->
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+include("footer.php")
+?>
