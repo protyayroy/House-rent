7bb4e29bc3b74d5c395803cfad5a5f03154c908e
Protyay Roy <98158272+Protyay-Roy@users.noreply.github.com>
2022-12-25 22:50:43 +0600

    Add files via upload

---
+++ owner/chat/fetch.php
@@ -0,0 +1,33 @@
+<?php
+include("../../config/config.php");
+
+if (isset($_POST["action"])) {
+
+    $receiverId = $_POST["receiverId"];
+    $senderEmail = $_POST["senderEmail"];
+
+    $senderQ = $db->query("SELECT * FROM owner WHERE email = '$senderEmail'");
+    if ($senderQ) {
+        $senderR = $senderQ->fetch_assoc();
+        $senderId = $senderR["owner_rand_id"];
+    }
+
+    $s = "SELECT * FROM message WHERE (send_id = '$senderId' && receive_id = '$receiverId') OR(send_id = '$receiverId' && receive_id = '$senderId') order by id";
+
+    $q = $db->query($s);
+
+    $output = "";
+
+    while ($r = $q->fetch_assoc()) {
+
+        if ($r["send_id"] === $senderId) {
+            $output .= "<div class='outgoing-msg text-end'> <p>" . $r["send_text"] . "</p> </div>";
+        } else {
+            $output .= "<div class='imcomming-msg'> <p> " . $r["send_text"] . " </p> </div>";
+        }
+    }
+
+    echo $output;
+} else {
+    echo 'ajax no';
+}
